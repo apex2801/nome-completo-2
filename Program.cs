f0c7ada616d0fd057660892f3b29e7971f7b7063
apex2801 <aapinheiro2005@gmail.com>
2021-03-14 20:38:01 -0300

    .

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace nome_completo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              Console.WriteLine("arthur"); 
+              Console.WriteLine("albuquerque"); 
+              Console.WriteLine("arthur albuquerque pinheiro"); 
+              Console.WriteLine("pinheiro, arthur");
+        }
+    }
+}
